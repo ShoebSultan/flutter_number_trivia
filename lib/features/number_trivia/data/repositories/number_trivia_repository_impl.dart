183d476716edd95cac07f3793592c6c11634306a
shoeb sultan <shoebsultan4111@gmail.com>
2022-01-11 03:37:21 +0530

    network info

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_number_trivia/core/error/exceptions.dart';
-import 'package:flutter_number_trivia/core/platform/network_info.dart';
+import 'package:flutter_number_trivia/core/network/network_info.dart';
 import 'package:flutter_number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
