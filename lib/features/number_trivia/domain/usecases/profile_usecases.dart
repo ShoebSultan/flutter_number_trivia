1f1282f977704093972aa1e7d6788763fc1911fa
shoeb sultan <shoebsultan4111@gmail.com>
2022-01-29 18:37:36 +0530

    this is abstract class  profile usecases

---
+++ lib/features/number_trivia/domain/usecases/profile_usecases.dart
@@ -0,0 +1,12 @@
+/* abstract class ProfileUseCases {
+  Future<User> onEditUserName(String name);
+  Future<User> onEditUserEmail(String email);
+
+  /// For User Profile
+  Future<User> onEditUserMobileNo(String mobile);
+  Future<User> editUserProfileImage(String image);
+  Future<User> editUserRole(String role);
+
+  Future<User> UserNotifications(String notification);
+}
+ */
